0e108a9932f9f40e2aca6ea90296e0819495929e
Chukwuemeka Ezumezu <emyfrancis2012@gmail.com>
2024-08-28 23:31:08 +0100

    testing the cicd pipeline

--- code/train/train.py
+++ code/train/train.py
@@ -14,6 +14,7 @@ from sklearn.model_selection import train_test_split
 from azureml.core import Dataset, Run
 run = Run.get_context()
 
+#test
 
 def log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix', title='Confusion matrix', cmap=plt.cm.Blues):
     '''
